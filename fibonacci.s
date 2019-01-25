40a28b6567c8c2f145e49dcfca32242ff92a51f9
Jack Spence <jack.c.spence@gmail.com>
2019-01-25 15:01:07 -0500

    Added print to fib function and fixed bug with dropping zeros mid string.

--- fibonacci.s
+++ fibonacci.s
@@ -1,10 +1,16 @@
 .intel_syntax noprefix
 
 .section	.rodata
-NUM1:
-		.asciz	"NUM1: 0x%lx%lx\n"
-NUM2:
-		.asciz	"NUM2: 0x%lx\n"
+first:
+		.asciz	"0x%.lx%.lx%.lx%.lx\n"
+second:
+		.asciz	"0x%.lx%.lx%.lx%lx\n"
+third:
+		.asciz	"0x%.lx%.lx%lx%016lx\n"
+fourth:
+		.asciz	"0x%.lx%lx%016lx%016lx\n"
+fifth:
+		.asciz	"0x%lx%016lx%016lx%016lx\n"
 
 .data
 index:		.quad 0
@@ -15,17 +21,24 @@ fibonacci:
 	call atoi	# Takes argv[1] and sends it to atoi
 	mov rcx, rax
 	cmp rcx, 0 	# Checks if 0 was on command line
-	je .no_count
+	je .2
 	mov rdx, 0
 	mov rax, 1
-	mov r10, 0
-	xor r15, r15
 	xor r8, r8
+	xor r9, r9
+	xor r10, r10
+	xor r11, r11
+	xor r12, r12
+	xor r15, r15
 .start_loop:
 	clc
 	xadd rax, rdx
 	adc r8, 0
 	xadd r15, r8
+	adc	r9, 0	
+	xadd r10, r9
+	adc	r11, 0	
+	xadd r12, r11
 
 .reloop:
 	dec rcx
@@ -38,7 +51,39 @@ fibonacci:
 	ret
 
 .end:
-	
+	mov rsi, r11
+	mov rcx, r8
+	mov rax, rdx
+	mov rdx, r9
+	mov r8, rax
+	cmp r8, 0
+	je .1
+	cmp rcx, 0
+	je .2
+	cmp rdx, 0
+	je .3
+	cmp rsi, 0
+	je .4
+	jmp .5
+.1:
+	mov rdi, OFFSET first # Preps format string
+	jmp .print
+.2:
+	mov rdi, OFFSET second	# Preps format string
+	jmp .print
+.3:
+	mov rdi, OFFSET third	# Preps format string
+	jmp .print
+.4:
+	mov rdi, OFFSET fourth	# Preps format string
+	jmp .print
+.5:
+	mov rdi, OFFSET fifth
+
+
+.print:
+	mov rax, 0	# Needed for printf
+	call printf
 	ret
 
 .globl	main
@@ -53,20 +98,8 @@ main:
 
 	add rsi, 0x8
 	mov rdi, 1
-	#mov [n2], rdi
 	mov rdi, [rsi]
 	call fibonacci
-	mov rsi, r8
-
-	mov rdi, OFFSET NUM1	# Preps format string
-	mov rax, 0	# Needed for printf
-	call printf
-
-	mov rsi, r8
-	mov rdi, OFFSET NUM2	# Preps format string
-	mov rax, 0	# Needed for printf
-	call printf
-
 
 	mov rbx, [rbp-0x18]
 	add rsp, 0x30
